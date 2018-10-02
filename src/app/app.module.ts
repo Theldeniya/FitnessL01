cbf3b2a5b0d70555785aa456e4ee82d053356af3
hashini <hashiniynet@gmail.com>
2018-10-02 11:43:35 +0530

    loginpage frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { NavComponent } from './nav/nav.component';
+import { FootComponent } from './foot/foot.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FootComponent,
+    SignuppageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
